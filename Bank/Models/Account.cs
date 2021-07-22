d2d35ffc054d8f5a7867c7b24cf14b16e418f936
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-07-21 22:20:34 -0300

    Add "Account" and "Event" model classes

---
+++ Bank/Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace Bank.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public float Balance { get; set; }
+    }
+}
---
+++ Bank/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank.Models
+{
+    public class Event
+    {
+        public string Type { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public float Amount { get; set; }
+    }
+}

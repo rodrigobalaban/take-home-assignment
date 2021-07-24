af27a4b9000cdece89c2d289a5209d5f4fde19d0
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-07-24 16:24:21 -0300

    Add "Balance" request
    The request returns the balance of valid account

---
+++ Bank/Controllers/BalanceController.cs
@@ -0,0 +1,31 @@
+﻿using Bank.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Bank.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BalanceController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BalanceController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetBalanceAccount(string account_id)
+        {
+            var account = await _accountService.GetAccountByIdAsync(account_id);
+            
+            if (account == null)
+            {
+                return NotFound(0);
+            }
+
+            return Ok(account.Balance);
+        }
+    }
+}
